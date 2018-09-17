57dd22536b0b196887b9ddb53b1759f2422b57b5
sasakazu <sasakura.company@gmail.com>
2018-09-17 19:52:35 +0900

    image done,,,,

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 
 
 <footer class="page-footer">
-    <p class="copyright"><small>©2018 Yourname</small></p>
+    <p class="copyright"><small>©2018 Sasakura.Company</small></p>
 </footer>
+
+
 <?php wp_footer(); ?>
+
+
 </body>
 </html>
