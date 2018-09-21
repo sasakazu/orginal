bf7e0dafd7d9a686c60fc17bc6e4fd19b007a4db
sasakazu <sasakura.company@gmail.com>
2018-09-21 13:43:00 +0900

    no boot start .....

--- front-page.php
+++ front-page.php
@@ -1,59 +1,6 @@
 <?php get_header(); ?>
 
 
-<div class="main-img">
-  <div class="bg-inner">
-    <h2><span>L</span>ogistic  <span>F</span>utsal  <span>A</span>parel</h2>
-    <p>- Bass Co., Ltd. -</p>
-  </div>
-</div>
-
-
-
-<div class="bussiness-back">
-
-<div class="container bussiness-wrapper">
-  <div class="row bussiness-main">
-
-    <div class="col-sm col-md logistic-main">
-      <div class="logi-wrapper">
-
-      <img src="<?php bloginfo('template_directory'); ?>/images/logi.png" alt="..." class="" width=100px" height=100px">
-      <h3>Logistic</h3>
-      <h4>物流部門</h4>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-      </div>
-    </div>
-
-
-    <div class="col-sm col-md futsal-main">
-      <div class="fut-wrapper">
-
-      <img src="<?php bloginfo('template_directory'); ?>/images/soccer.png" alt="..." class="" width=130px" height=100px">
-      <h3>Futsal</h3>
-      <h4>フットサル事業</h4>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-    </div>
-    </div>
-
-    <div class="col-sm col-md aparel-main">
-
-      <div class="apa-wrapper">
-
-        <img src="<?php bloginfo('template_directory'); ?>/images/dad.png" alt="..." class="" width=100px" height=100px">
-        <h3>Aparel</h3>
-        <h4>アパレル事業</h4>
-        <p>テキストテキストテキストストテキストストテキストストテキストストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-
-      </div>
-
-    </div>
-
-  </div>
- </div>
-
-</div>
-
 
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -5,36 +5,22 @@
         <title><?php wp_title( ' | ', true, 'right' ); ?></title>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-
-
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="#">
-    <?php bloginfo( $name ); ?>
 
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+      <div class="topnav">
+        <div class="nav-title">
+          <h1><a href=""><?php bloginfo( $show ); ?></a></h1>
 
-    <a class="nav-item nav-link" href="#">物流事業</a>
-    <a class="nav-item nav-link" href="#">フットサル事業</a>
-    <a class="nav-item nav-link" href="#">アパレル事業</a>
-    <a class="nav-item nav-link" href="#">Bassについて</a>
-    <a class="nav-item nav-link" href="#">お問い合わせ</a>
+        </div>
 
-    </div>
+        <ul>
+          <li><a href="">SERVISE</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
 
-  </div>
 
-</nav>
+      </div>
